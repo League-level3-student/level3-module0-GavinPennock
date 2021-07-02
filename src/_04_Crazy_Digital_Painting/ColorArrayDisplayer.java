b1742b08c136755e4b5e7fa8d247160ed8cb28c0
league <league@imac-13.attlocal.net>
2021-07-01 18:27:13 -0700

    finished everything from firework display to crazy digital painting

--- src/_04_Crazy_Digital_Painting/ColorArrayDisplayer.java
+++ src/_04_Crazy_Digital_Painting/ColorArrayDisplayer.java
@@ -12,6 +12,7 @@ import javax.swing.JLabel;
  */
 
 public class ColorArrayDisplayer {
+
     public static void displayColorsAsImage(Color[][] colors) {
         JFrame window = new JFrame();
         JLabel label = new JLabel();
